efb59eba8fb374e38894c08cea2322821021d2bd
Abhishek <abhishek@Igniva-Designer-Nitinsareen.local>
2018-04-04 15:31:42 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Text>Changes you make will automatically reload.</Text>
+        <Text>Abhishek is in the house.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'teal',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
