be9d3d8bd865ef851532fa11a2e17b56e0b45061
Abhishek <abhishek@Igniva-Designer-Nitinsareen.local>
2018-04-17 19:07:24 +0530

    swipper

--- src/screens/DetailsScreen.js
+++ src/screens/DetailsScreen.js
@@ -1,14 +1,124 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet,Button,Image } from 'react-native';
+import Swiper from 'react-native-deck-swiper';
 
+let obj1={
+    left: {
+        element: <Text>NOPE</Text> ,/* Optional */
+        title: 'NOPE',
+          style: {
+            label: {
+              backgroundColor: 'black',
+              borderColor: 'black',
+              color: 'white',
+              borderWidth: 1
+            },
+            wrapper: {
+              flexDirection: 'column',
+              alignItems: 'flex-end',
+              justifyContent: 'flex-start',
+              marginTop: 30,
+              marginLeft: -30
+            }
+          }
+        },
+      right: {
+        element: <Text>LIKE</Text> ,/* Optional */
+        title: 'LIKE',
+          style: {
+            label: {
+              backgroundColor: 'black',
+              borderColor: 'black',
+              color: 'white',
+              borderWidth: 1
+            },
+            wrapper: {
+              flexDirection: 'column',
+              alignItems: 'flex-start',
+              justifyContent: 'flex-start',
+              marginTop: 30,
+              marginLeft: 30
+            }
+    }
+  }
+}
+let obj2 = {
+  position: 'absolute',
+  backgroundColor: 'transparent',
+  zIndex: 2,
+  flex: 1,
+  width: '100%',
+  height: '100%'
+}
 class DetailsScreen extends React.Component {
   render() {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Code Push Done Right</Text>
+      <View style={{ flex: 1 ,backgroundColor: "#F5FCFF"}}>
+        <Swiper
+            cards={['DO', 'MORE', 'OF', 'WHAT', 'MAKES', 'YOU', 'HAPPY']}
+            renderCard={(card) => {             
+                return (
+                    <View style={styles.card}>
+                      <Image style={{width: 66, height: 58}} source={{uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png'}}/>
+                        <Text style={styles.text}>{card}</Text>
+                    </View>
+                )
+            }}
+            onSwiped={(cardIndex) => {console.log(cardIndex)}}
+            onSwipedAll={() => {console.log('onSwipedAll')}}
+            cardIndex={0}
+            backgroundColor={'#4FD0E9'}
+            stackSize= {2}
+            showSecondCard={true}
+            verticalSwipe ={false}
+            onSwipedLeft ={()=>{}}
+            onSwipedRight ={()=>{}}
+            overlayLabels ={obj1}
+            overlayLabelWrapperStyle={obj2}
+            overlayLabelStyle={styles.overlayLabelStyle}
+            >
+           
+        </Swiper> 
+        <View style={{backgroundColor:'red',position:'absolute',bottom:10,left:10,right:10,flexDirection:'row'}}> 
+            <Button
+                    onPress={() => {console.log('oulala')}}
+                    title="Press me">
+                    You can press me
+                </Button> 
+                <Button
+                    onPress={() => {console.log('oulala')}}
+                    title="Press me">
+                    You can press me
+                </Button>      
+          </View>
       </View>
     );
   }
 }
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  },
+  overlayLabelStyle:{
+    fontSize: 45,
+    fontWeight: 'bold',
+    borderRadius: 10,
+    padding: 10,
+    overflow: 'hidden'
+  },
+  card: {
+    height:300,
+    borderRadius: 4,
+    borderWidth: 2,
+    borderColor: "#E8E8E8",
+    justifyContent: "center",
+    backgroundColor: "white"
+  },
+  text: {
+    textAlign: "center",
+    fontSize: 50,
+    backgroundColor: "transparent"
+  }
+});
 export default DetailsScreen;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Text, View,TouchableOpacity } from 'react-native';
-
+import LinkedInModal from 'react-native-linkedin'
+// https://www.linkedin.com/developer/apps link to create linkedin developer account
 class HomeScreen extends React.Component {
     render() {
       return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>Home Screen</Text>
+          <View style={{backgroundColor: '#fff',
+                justifyContent: 'center',
+                alignItems: 'center'}}>
+            <LinkedInModal
+              clientID="[ Your client id from https://www.linkedin.com/developer/apps ]"
+              clientSecret="[ Your client secret from https://www.linkedin.com/developer/apps ]"
+              redirectUri="[ Your redirect uri set into https://www.linkedin.com/developer/apps ]"
+              onSuccess={token => console.log(token)}
+            />
+          </View>          
           <TouchableOpacity onPress={()=>{this.props.navigation.navigate('Details')}} style={{backgroundColor:'lightgreen',padding:10}}><Text>Press Me</Text></TouchableOpacity>
         </View>
       );
